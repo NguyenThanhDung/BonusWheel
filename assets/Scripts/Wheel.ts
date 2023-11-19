e7e487699f047556bdf58a00a96d0e151a609245
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-11-18 18:50:19 -0600

    Instantiate sector prefab

--- assets/Scripts/Wheel.ts
+++ assets/Scripts/Wheel.ts
@@ -1,9 +1,15 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, instantiate, Node, Prefab } from 'cc';
 import { Sector } from './Sector';
 const { ccclass, property } = _decorator;
 
 @ccclass('Wheel')
 export class Wheel extends Component {
+
+    @property({
+        type: Prefab,
+        tooltip: 'Sector Prefab'
+    })
+    public sectorPrefab : Prefab;
 
     private sector: Sector[];
     private angle: number;
@@ -13,6 +19,9 @@ export class Wheel extends Component {
         this.sector = [];
         this.angle = 0;
         this.speed = 20;
+
+        var node = instantiate(this.sectorPrefab);
+        node.parent = this.node;
     }
 
     update(deltaTime: number) {
